400e4644876ee5747322e542ed960648362dbed7
Hamidreza Montaseri <hr.montaseri@gandom.co>
2019-08-18 01:45:11 +0430

    Use dataset API

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,45 +1,54 @@
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark._
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010._
-import com.datastax.spark.connector._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, StringType, StructType}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val Array(zkQuorum, topics, groupID, cassandraHost) = args
+    val Array(kafkaHost, topics, cassandraHost) = args
 
-    val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> zkQuorum,
-      "key.deserializer" -> classOf[StringDeserializer],
-      "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> groupID,
-      "auto.offset.reset" -> "earliest",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
-    )
+    val schema = new StructType()
+      .add("id",StringType)
+      .add("latitude",DoubleType)
+      .add("longitude",DoubleType)
+      .add("time",StringType)
 
-    val conf = new SparkConf()
-      .setAppName("DigestData")
-      .set("spark.cassandra.connection.host", cassandraHost)
+    val spark = SparkSession
+      .builder
+      .appName("DigestData")
+      .config("spark.cassandra.connection.host", cassandraHost)
+      .getOrCreate()
 
-    val streamingContext = new StreamingContext(conf, Seconds(1))
-    val topicArr = topics.split(",")
+    import spark.implicits._
 
-    val stream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
-      PreferConsistent,
-      Subscribe[String, String](topicArr, kafkaParams)
-    )
+    val dataFrame = spark
+      .read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", kafkaHost)
+      .option("subscribe", topics)
+      .option("startingOffsets", "latest")
+      .load()
+      .selectExpr("CAST(value AS STRING)").as[String]
 
-    val values = stream.map(record => record.value())
-    values.foreachRDD(rdd => {
-      val crdd = rdd.map(value => Tuple1(value))
-      crdd.saveToCassandra("sparkData", "vehicleLocation", SomeColumns("location"))
-    })
+    dataFrame.printSchema()
+    val jsons = dataFrame.select(from_json($"value", schema) as "data").select("data.*")
 
-    streamingContext.start()             // Start the computation
-    streamingContext.awaitTermination()  // Wait for the computation to terminate
+    val query = jsons.writeStream
+      .foreachBatch { (batchDF, _) =>
+        batchDF
+          .write
+          .format("org.apache.spark.sql.cassandra")
+          .option("keyspace", "sparkdata")
+          .option("table", "detaillocation")
+          .mode("append")
+          .save
+      }.start
+
+    val debug = jsons.writeStream
+        .outputMode("append")
+        .format("console")
+        .start
+
+    debug.awaitTermination
+    query.awaitTermination
   }
-
 }
